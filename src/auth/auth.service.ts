43178f033b703fba1745cce0b1a9e23c95008645
FelipeShimada <felipe.shimada@kepha.com.br>
2025-08-19 16:53:48 -0300

    Add Prisma and NestJS dependencies, configure PrismaService and global validation
    - Added @nestjs/jwt, bcrypt, class-transformer, and class-validator
    - Removed test files and unnecessary configs
    - Updated .gitignore to ignore Prisma-generated files
    - Implemented PrismaService and enabled global validation

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto, RegisterDto } from './auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private jwtService: JwtService
+    ) { }
+
+    async login(loginDto: LoginDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: loginDto.email,
+            },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const accessToken = await this.jwtService.signAsync({
+            userId: user.id,
+            email: user.email,
+            name: user.name
+        }, { expiresIn: '1h' });
+
+        return {
+            accessToken,
+            message: 'Login successful',
+            data: loginDto,
+        };
+    }
+
+    async register(registerDto: RegisterDto) {
+
+        const userAlreadyExists = await this.prisma.user.findUnique({
+            where: {
+                email: registerDto.email,
+            },
+        });
+
+        if (userAlreadyExists) {
+            throw new BadRequestException('User already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+        const user = await this.prisma.user.create({
+            data: {
+                ...registerDto,
+                password: hashedPassword,
+            },
+        });
+
+        return {
+            message: 'Register successful',
+            data: user,
+        };
+    }
+}
